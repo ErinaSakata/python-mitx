3e774430342c56b53a99de20e543a8b133323c78
Hassan Abid <hassanabidpk89@gmail.com>
2015-01-10 15:19:24 +0900

    added Chapter 3 practice problem 1
    Helpful tips
    
    1-  Style Guide for Python Code
    https://www.python.org/dev/peps/pep-0008/
    
    2 - A module's __name__   a-
    http://ibiblio.org/g2swap/byteofpython/read/module-name.html
    b- http://stackoverflow.com/questions/419163/what-does-if-name-main-do

---
+++ HASSAN/chap3_1.py
@@ -0,0 +1,28 @@
+"""Returns the perfect cube root of an integer x
+
+if x is not perfect square then it displays a message 
+	
+"""
+
+def cube_root(x):
+
+	# x = int(raw_input("Enter integer : "))
+	ans = 0
+
+	while ans**3 < abs(x):
+		ans = ans + 1
+
+	if ans**3 != abs(x):
+		print x, "is not perfect cube"
+	else:
+		if x < 0:
+			ans = -ans
+
+		print "Cube root of", x," is : ",ans
+
+
+if __name__ == "__main__":
+	cube_root(10)
+	cube_root(5)
+	cube_root(9)
+	cube_root(27)
