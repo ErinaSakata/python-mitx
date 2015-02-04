1736eacaadd08a311383b62d27fe53eb1b116c3d
Olga Tegay <olga.tegay@gmail.com>
2015-02-04 20:38:29 +0900

    made changes to Hello World file

---
+++ Olga/Finger4.py
@@ -0,0 +1,36 @@
+total = 0
+s = "1.23,2.4,3.123"
+s.split(",")
+print s
+
+	#total = total + float(n)
+#print total
+
+"""total = 0
+for c in "1234567":
+	total = total + int(c)
+print total"""
+
+"""s = "1.24"
+d = float(s)
+print type(d)"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
